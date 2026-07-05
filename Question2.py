6d6c83ed75d939248c653c4988346f576f2d3a24
Fatine-I <f.icyimpaye@alustudent.com>
2026-07-05 21:00:11 +0400

    writing to Question2 file

--- Question2.py
+++ Question2.py
@@ -1,1 +1,7 @@
 #the pirate's Age
+print("Welcome to the PYTHON ISLAND 🏴‍☠️!!!\n\n\n")
+user_name=input("Introduce yourself...\n")
+print("Warm Welcome", user_name, "on the PYTHON ISLAND 🏴‍☠️")
+
+user_age=input(user_name,"what is your Age?\n")
+print(user_name, "is",user_age, "years old.")
