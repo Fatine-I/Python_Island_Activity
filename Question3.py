d285b31f17b18d8314aa9856022ab5a74ecd58be
Fatine-I <f.icyimpaye@alustudent.com>
2026-07-05 22:27:21 +0400

    committing changes to branch_5mark

--- Question3.py
+++ Question3.py
@@ -3,9 +3,12 @@ print("Welcome to the PYTHON ISLAND 🏴‍☠️!!!\n\n\n")
 user_name=input("Introduce yourself...\n")
 print("Warm Welcome", user_name, "on the PYTHON ISLAND 🏴‍☠️")
 
-user_age=input(user_name,"what is your Age?\n")
+print(user_name,"what is your Age?\n")
+user_age=input('Age:')
 print(user_name, "is",user_age, "years old.")
 
-fav_colour=input(user_name,"what is your favorite color?\n")
-fav_animal=input(user_name, "what is your favorite animal?\n")
+print(user_name,"what is your favorite color?\n")
+fav_colour=input('Color:')
+print("favorite animal?\n")
+fav_animal=input('Animal:')
 pirate_name=fav_colour+" "+fav_animal # Swashbuckler way of naming pirates
--- Question6.py
+++ Question6.py
@@ -3,20 +3,24 @@ print("Welcome to the PYTHON ISLAND 🏴‍☠️!!!\n\n\n")
 user_name=input("Introduce yourself...\n")
 print("Warm Welcome", user_name, "on the PYTHON ISLAND 🏴‍☠️")
 
-user_age=input(user_name,"what is your Age?\n")
+print(user_name,"what is your Age?\n")
+user_age=input('Age:')
 print(user_name, "is",user_age, "years old.")
 
-fav_colour=input(user_name,", what is your favorite color?\n")
-fav_animal=input(user_name, ", what is your favorite animal?\n")
+print(user_name,"what is your favorite color?\n")
+fav_colour=input('Color:')
+print("favorite animal?\n")
+fav_animal=input('Animal:')
 pirate_name=fav_colour+" "+fav_animal # Swashbuckler way of naming pirates
 
-coins=input(user_name, ", how many coins did you get?\n")
+print(user_name, ", how many coins did you get?\n")
+coins=int(input('Coins:'))
 print(user_name, "has", coins, "coins")
 
-pirates=input("How many pirates are on the crew though?\n")
+pirates=int(input("How many pirates are on the crew though?\n"))
 share_per_pirate=coins/pirates
 
-if shar_per_pirate==15:
+if share_per_pirate>=15:
     print("Yeeei!!! We have happy pirates on the crew")
 else:
     print( "Awwwch!!! We don't have big coins yet. Yet to have!")
